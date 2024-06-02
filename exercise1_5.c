3eaad88627e10479d399d30e267015a9ebb7131e
Adam Fitzgerald <adam@networknoob.net>
2024-06-02 08:21:40 +0000

    exercise 1.5

--- exercise1_5.c
+++ exercise1_5.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+// define is declaration of symbolic name or constant (variable)
+
+#define LOWER   0       /* lower limit of table */
+#define UPPER   300     /* upper limit */
+#define STEP    20      /* step size */
+
 /* print Farenheit-Celsius table */
 
 int main(){
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
